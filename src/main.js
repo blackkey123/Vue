f4a10975f9af149ccd1f04ca6897c445c72a3fdf
杨晓东 <54590363+blackkey123@users.noreply.github.com>
2020-07-14 10:44:12 +0800

    vue

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import "mint-ui/lib/style.css"
 import Axios from "axios"
 import Global from "./network/Global.vue"
 Vue.prototype.$GLOBAL = Global//挂载到Vue实例上面
+
 Vue.prototype.$axios = Axios;
 Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
