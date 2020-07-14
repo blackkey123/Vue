286921e5cdc2b00ff52dbf1bad0332e1ee141007
杨晓东 <54590363+blackkey123@users.noreply.github.com>
2020-07-14 10:44:57 +0800

    vue1212131

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import Axios from "axios"
 import Global from "./network/Global.vue"
 Vue.prototype.$GLOBAL = Global//挂载到Vue实例上面
 
+
 Vue.prototype.$axios = Axios;
 Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
